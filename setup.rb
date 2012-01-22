98a97ff5d6da68d77de34602a6e865bdc3ea5404
Jingjing Duan <duanjingjing@gmail.com>
2012-01-22 18:43:12 -0500

    setup.rb: force delete existing files and reformat

--- setup.rb
+++ setup.rb
@@ -3,18 +3,20 @@
 # ignore files below:
 #   .git
 
+require "fileutils"
+
 target_dir = Dir.home + "/dotfiles/"
 src_dir = Dir.home + "/"
 Dir.chdir(target_dir)
 Dir.glob('.*').each do |file|
-    if (file != "." && file != ".." && file != ".git")
-        src_path = src_dir + file
-        target_path = target_dir + file
-        if File.exists?(src_path)
-            puts "symlink #{src_path} exists already!"
-        else 
-            puts "create symlink: #{src_path}"
-            `ln -s #{target_path} #{src_path}`
-        end
+  if (file != "." && file != ".." && file != ".git")
+    src_path = src_dir + file
+    target_path = target_dir + file
+    if File.exists?(src_path)
+      FileUtils.rm_rf(src_path)
+      puts "#{src_path} exists already, just deleted"
     end
+    puts "create symlink: #{src_path}"
+    `ln -s #{target_path} #{src_path}`
+  end
 end
