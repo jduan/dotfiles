f3344f29158fc1def43a6684fededc84e5a82fdc
Jingjing Duan <jduan@fitbit.com>
2017-08-01 17:05:57 -0700

    disable a few things in config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,11 +1,11 @@
-set -x PATH $PATH /Users/jduan/.local/bin /Users/jduan/bin
-set -x JAVA_HOME (/usr/libexec/java_home -v 1.8)
+# set -x PATH $PATH /Users/jduan/.local/bin /Users/jduan/bin
+# set -x JAVA_HOME (/usr/libexec/java_home -v 1.8)
 
 source ~/.fish_aliases
 source ~/.config/fish/functions/ctrl_f.fish
 
 fish_vi_key_bindings
-set -g fish_user_paths "/usr/local/opt/node@6/bin" $fish_user_paths
+# set -g fish_user_paths "/usr/local/opt/node@6/bin" $fish_user_paths
 
 # set up rbenv
-status --is-interactive; and . (rbenv init -|psub)
+# status --is-interactive; and . (rbenv init -|psub)
