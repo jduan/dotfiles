fa92df7d06aef14a2dad97d8f24fb41eb46e41dc
Jingjing Duan <jduan@fitbit.com>
2017-08-12 21:10:44 -0700

    enable rbenv

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,5 +7,5 @@ source ~/.config/fish/functions/ctrl_f.fish
 fish_vi_key_bindings
 # set -g fish_user_paths "/usr/local/opt/node@6/bin" $fish_user_paths
 
-# set up rbenv
-# status --is-interactive; and . (rbenv init -|psub)
+set up rbenv
+status --is-interactive; and . (rbenv init -|psub)
