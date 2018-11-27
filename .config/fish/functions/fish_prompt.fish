65c4d9d2b046cec7a0d4c009d32d6f2af1fd1e88
Jingjing Duan <duanjingjing@gmail.com>
2018-11-27 09:50:23 -0800

    remove extra space in shell prompt

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -40,12 +40,14 @@ function fish_prompt
   set last_status $status
 
   set_color $fish_color_cwd
-  printf '%s ' (prompt_pwd)
+  printf '%s' (prompt_pwd)
   set_color normal
 
   set BIG_REPOS treehouse fullhouse airbnb
   if not contains (basename $PWD) $BIG_REPOS
     printf '%s ' (__fish_git_prompt)
+  else
+    printf ' '
   end
 
   set_color normal
